681992a3fa49fc6b29294f1803a309f0341da3bd
Varelas Panagiotis <varelaspanos@gmail.com>
2017-05-19 22:44:46 +0300

    added function for retrieving actuall microcontroller signature
    Signed-off-by: Varelas Panagiotis <varelaspanos@gmail.com>

--- dude.cpp
+++ dude.cpp
@@ -14,8 +14,8 @@ void Dude::setup ( gboolean auto_erase, gboolean auto_verify, gboolean auto_chec
         protocol.clear();
         options.clear();
 
-        //cout << microcontroller << " on " << programmer << endl;
-
+        /* set device parameter */
+        device.append(" -p ");
         /* guess family and device name */
         if (microcontroller.find ("ATtiny", 0) == 0) {
                 device.append("t");
@@ -35,12 +35,9 @@ void Dude::setup ( gboolean auto_erase, gboolean auto_verify, gboolean auto_chec
                 device.append("usb");
                 device.append(microcontroller.substr(7, 32));
         }
-        cout << "device name: -" << device << "-" << endl;
 
-
-
-        /* keep protocol in private member */
-        protocol.append(programmer);
+        /* set protocol parameter */
+        device.append(" -c ").append(programmer).append(" ");
 
         /* check if should disable automatic erase of flash */
         if (!auto_erase)
@@ -56,11 +53,45 @@ void Dude::setup ( gboolean auto_erase, gboolean auto_verify, gboolean auto_chec
         /* disable progress-bars */
         options.append("-q ");
 
+        //cout << "protocol: " << protocol << endl;
+        //cout << "device: " << device << endl;
+        //cout << "options: " << options<< endl;
 }
 
 
+Glib::ustring Dude::get_signature (void)
+{
+        Glib::ustring command, output, actual_signature;
+
+        /* prepare command to be executed */
+        command.append("avrdude");
+        command.append(device);
+        command.append(protocol);
+        command.append(options).append("-F ");
+        /* execute command and get output */
+        output = execute (command);
+        /* find signature location in output */
+        Glib::ustring::size_type sig_pos = output.find ("signature = 0x", 0);
+        /* extract signature */
+        actual_signature = output.substr (sig_pos + 14, 6);
+
+        cout << "\n\nraw output: " << output << "\n\n" << endl;
+        cout << "executed command: " << command << endl;
+        cout << "extracted signature: " << actual_signature << endl;
+
+        return actual_signature;
+}
 
 
+Glib::ustring Dude::device_erase (void)
+{
+        Glib::ustring command, output;
+
+        /* execute command and get output */
+        output = execute (command);
+
+        return output;
+}
 
 Glib::ustring Dude::execute (Glib::ustring command)
 {
